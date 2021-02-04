d34f6b4f3e554298134f64ba83e36db1eb4bb1fb
David <kdavidk98@gmail.com>
2021-02-04 12:09:32 -0300

    changing indentation, accuracy calculation

--- KNN.py
+++ KNN.py
@@ -2,10 +2,10 @@ import pandas as pd
 from math import sqrt
 
 def main():
-	dataset = pd.read_csv('Dataset_CancerClassification/Dados_Normalizados/cancer_train.csv')
-	
-	test = pd.read_csv('Dataset_CancerClassification/Dados_Normalizados/cancer_test.csv')
-	KNNEvaluate(3,dataset,test)
+    dataset = pd.read_csv('Dataset_CancerClassification/Dados_Normalizados/cancer_train.csv')
+    
+    test = pd.read_csv('Dataset_CancerClassification/Dados_Normalizados/cancer_test.csv')
+    KNNEvaluate(1,dataset,test)
 
 def euclideanDistance(row1, row2):
     distance = 0.0
@@ -15,32 +15,40 @@ def euclideanDistance(row1, row2):
     return sqrt(distance)
 
 def KNNEvaluate(KValue,dataset,test):
-	for testRowIndex, testRow in test.iterrows():
-		KNearestNeighbors = getNearestNeighbors(KValue,testRow,dataset)
-		testRowPredictedValue = getMedianValue(KNearestNeighbors, KValue)
-		if testRowPredictedValue == testRow['target']:
-			print("acertou1")
-		else: print("errou")
+    totalTests = 0
+    correctCount = 0
+    for testRowIndex, testRow in test.iterrows():
+        KNearestNeighbors = getNearestNeighbors(KValue,testRow,dataset)
+        testRowPredictedValue = getMedianValue(KNearestNeighbors, KValue)
+        totalTests+=1
+
+        if testRowPredictedValue == testRow['target']:
+            correctCount+=1
+            print("acertou")
+        else: 
+            print("errou")
+    print("acertou " + str(correctCount) + " de " + str(totalTests))
+    print("taxa de acerto de " + str(correctCount*100 / totalTests) + "%")
 
 def getNearestNeighbors(KValue,targetRow,dataset):
-	distances = list()
-	for dataRowIndex, dataRow in dataset.iterrows():
-		dist = euclideanDistance(targetRow.drop('target'), dataRow.drop('target'))
-		distances.append((dataRow, dist))
-	distances.sort(key=lambda tup: tup[1])
-	neighbors = list()
-	for i in range(KValue):
-		neighbors.append(distances[i][0])
-	return neighbors
+    distances = list()
+    for dataRowIndex, dataRow in dataset.iterrows():
+        dist = euclideanDistance(targetRow.drop('target'), dataRow.drop('target'))
+        distances.append((dataRow, dist))
+    distances.sort(key=lambda tup: tup[1])
+    neighbors = list()
+    for i in range(KValue):
+        neighbors.append(distances[i][0])
+    return neighbors
 
 def getMedianValue(nearNeighbors, neighborCount):
-	neighborsTotal = 0
-	for neighbor in nearNeighbors:
-		neighborsTotal += neighbor['target']
-	print (neighborsTotal)
-	if neighborsTotal >= neighborCount/2:
-		return 1.0
-	else: return 0.0
+    neighborsTotal = 0
+    for neighbor in nearNeighbors:
+        neighborsTotal += neighbor['target']
+    print (neighborCount/2)
+    if neighborsTotal >= neighborCount/2:
+        return 1.0
+    else: return 0.0
 
 
 main()
